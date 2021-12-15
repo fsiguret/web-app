901bcd8dd1cf77b364f2ca0a5f6596e8e794e66c
fsiguret <fl.si@orange.fr>
2021-12-15 23:10:08 +0100

    style: style for the header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,79 @@
-<script setup></script>
+<script setup>
+	const currentDate = new Date(Date.now());
+	const options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit',
+	};
+
+	const date = currentDate.toLocaleDateString('fr-FR', options);
+
+	function displayMenu(event) {}
+	function addTask() {
+		console.log('lol');
+	}
+</script>
 
 <template>
-	<header></header>
+	<header>
+		<div @click="displayMenu" class="hamburger">
+			<div class="hamburger__line"></div>
+			<div class="hamburger__line"></div>
+			<div class="hamburger__line"></div>
+		</div>
+		<div class="titleBlock">
+			<h1 class="titleBlock__title">Ma Liste</h1>
+			<p class="titleBlock__date">{{ date }}</p>
+		</div>
+		<form class="form" @submit.prevent="addTask">
+			<input class="form__input" type="text" placeholder="Ajouter une tâche" />
+			<span></span>
+		</form>
+	</header>
 	<main>
 		<router-view />
 	</main>
-	<aside></aside>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+	header {
+		display: flex;
+		flex-direction: column;
+		.hamburger {
+			cursor: pointer;
+			margin: 2em auto 2em 2em;
+			&__line {
+				width: 1.5rem;
+				height: 4px;
+				background-color: black;
+				&:not(:last-child) {
+					margin-bottom: 3px;
+				}
+			}
+		}
+		.titleBlock {
+			margin: 0 auto 0.5em 2em;
+			&__title {
+				font-size: 2.5em;
+				font-weight: bold;
+				line-height: normal;
+			}
+			&__date {
+				text-transform: capitalize;
+				font-size: 0.9em;
+			}
+		}
+		.form {
+			margin: 0 1.5em 0 1.5em;
+			position: relative;
+			&__input {
+				width: 100%;
+				padding: 0.7em;
+			}
+		}
+	}
+</style>
--- src/pages/TasksListView.vue
+++ src/pages/TasksListView.vue
@@ -2,6 +2,10 @@
 	<p>Salut</p>
 </template>
 
-<script setup></script>
+<script setup>
+	//aller chercher si il y a des tâches dans une base de données fictive (JSON)
+	//afficher la liste des tâches si il y en a
+	//sinon afficher qu'on a déjà fait toutes les tâches
+</script>
 
 <style scoped></style>
