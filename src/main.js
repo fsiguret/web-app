033f6af8746645a74a2c61a00e6a705611952f19
fsiguret <fl.si@orange.fr>
2021-12-21 15:46:37 +0100

    feat: add store with pinia

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import Router from './router/router.js';
 import App from './App.vue';
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
 app.use(Router);
+app.use(createPinia());
 app.mount('#app');
