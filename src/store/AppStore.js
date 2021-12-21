46e6a40237bb6e37f60de4c94eabcda40e97b88f
fsiguret <fl.si@orange.fr>
2021-12-21 17:33:55 +0100

    feat: add store for tasks

--- src/store/AppStore.js
+++ src/store/AppStore.js
@@ -3,10 +3,14 @@ import { defineStore } from 'pinia';
 export const useAppStore = defineStore('application', {
 	state: () => ({
 		menuIsActive: false,
+		addTaskIsActive: false,
 	}),
 	actions: {
 		switchMenu() {
 			this.menuIsActive = !this.menuIsActive;
+		},
+		wantAddTask() {
+			this.addTaskIsActive = !this.addTaskIsActive;
 		},
 	},
 });
